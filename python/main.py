045f56f2149b6d6528f8d077ac54c732848ff2ff
Zachary R <76828749+zchryr@users.noreply.github.com>
2023-10-17 17:43:56 -0400

    Snyk <> Python

--- python/main.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-not_a_password = "Hunter2"
-
-user_input = input("input: ")
-subprocess.run([user_input])
-
-print("Hello world!")
---
+++ python/script-1/main.py
@@ -0,0 +1,12 @@
+import subprocess
+
+password = "Hunter2"
+
+# These aren't real credentials.
+iam_access_key = "ALIUHALSJENRAIUSDFAL"
+iam_secret_access_key = "aASadFAEGrhtUJAet+AdgatwraaWEgCbthJawego"
+
+user_input = input("input: ")
+subprocess.run([user_input])
+
+print("Hello world!")
